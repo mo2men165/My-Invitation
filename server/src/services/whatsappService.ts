a0c14c6b06fd160fb5d569eebcd190d90d87aaac
mo2men165 <moamenabdeltawab@gmail.com>
2025-10-14 21:13:40 +0300

    remove paramters field

--- server/src/services/whatsappService.ts
+++ server/src/services/whatsappService.ts
@@ -516,23 +516,11 @@ export class WhatsappService {
               type: 'button',
               sub_type: 'quick_reply',
               index: 0,
-              parameters: [
-                {
-                  type: 'text',
-                  text: 'تأكيد الحضور'
-                }
-              ]
             },
             {
               type: 'button',
               sub_type: 'quick_reply',
-              index: 1,
-              parameters: [
-                {
-                  type: 'text',
-                  text: 'اعتذار عن الحضور'
-                }
-              ]
+              index: 1, 
             }
           ]
         }
